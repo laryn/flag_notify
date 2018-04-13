d5b6e9f4f93ae101a1fcd15d64d7b4ef6677c0a5
Antonello Lobianco <blackhole@lobianco.org>
2018-04-13 12:14:11 +0200

    Recopied 7.x-1.1 and updated documentation link
    As the commit of 7.x-1.1 was without any branch, recopied it here. Also updated the link to documentation.

--- flag_notify.api.php
+++ flag_notify.api.php
@@ -30,6 +30,7 @@ function hook_flag_notify_event_alter(&$break, &$event, &$groups, &$node, &$comm
 
 /**
  * Alter the parameters of the notification.
+ * New flag_notify_7.x-1.1: DEPRECATED function.. use hook_flag_notify_message_alter() instead
  * 
  * Possible [notification-type] / [event-type] matches:
  * - comment - new_comment
@@ -58,8 +59,48 @@ function hook_flag_notify_event_alter(&$break, &$event, &$groups, &$node, &$comm
  *   The uid of the user to notify.
  * @param string $notification_type
  *   The reason this user is notified, either 'comment', 'content' or 'group'.
+ *
  */
 function hook_flag_notify_notification_alter(&$break, &$event, &$group, &$node, &$comment, &$user, &$targetuser_uid, &$notification_type) {
+
+}
+
+/**
+ * Alter the parameters (including the message) of the notification.
+ * 
+ * Possible [notification-type] / [event-type] matches:
+ * - comment - new_comment
+ * - content - new_content
+ * - content - edited_content
+ * - group - new_comment
+ * - group - new_content
+ * - group - edited_content
+ * - group - new_users
+ *
+ * @param bool $break
+ *   An option to break the notification process.
+ * @param string $event
+ *   Type of event issued, either 'new_comment', 'new_content',
+ *   'edited_content' or 'new_user'.
+ * @param entity $group
+ *   The og groups to which the content and the notified user belong to.
+ * @param entity $node
+ *   The content edited or with new comments.
+ * @param entity $comment
+ *   The comment sent (if any).
+ * @param entity $user
+ *   The comment or content author (new comment/content), acting user (edited
+ *   content) or the new og user.
+ * @param entity $tuser
+ *   The target user to notify.
+ * @param string $notification_type
+ *   The reason this user is notified, either 'comment', 'content' or 'group'.
+ * @param string $message_subject
+ *   The subject of the message to be altered. Use the other parameters of this function to build your own notification message
+ * @param string $message_body
+ *   The body of the message to be altered. Use the other parameters of this function to build your own notification message
+ */
+function hook_flag_notify_message_alter(&$break, &$event, &$group, &$node, &$comment, &$user, &$tuser, &$notification_type, &$message_subject, &$message_body) {
 
 }
 
